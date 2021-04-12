2fcdd9e7b2e153d958f5ee686a24d57a579db0dc
rajesh17 <g.rajesh1234@gmail.com>
2021-04-12 08:28:50 -0500

    Initial Commit

---
+++ src/com/rajesh/examples/Counter.java
@@ -0,0 +1,11 @@
+package com.rajesh.examples;
+
+class Counter {
+    int count = 0;
+    public synchronized void increment() {
+        count = count + 1;
+    }
+    public int getCount() {
+        return count;
+    }
+}
